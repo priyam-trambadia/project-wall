98d8f7b446a032f9b4cad653810c72acbade86e5
Priyamkumar Trambadia <priyamcancode@gmail.com>
2024-07-20 23:11:02 +0530

    Added new error: Invalid User,Invalid Project and Mail

--- api/utils/error.go
+++ api/utils/error.go
@@ -10,10 +10,66 @@ func RenderInternalServerErr(w http.ResponseWriter) {
 	)
 }
 
+func RenderEmailErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Currently server is unable to send emails. Please try again later.",
+		http.StatusServiceUnavailable,
+	)
+}
+
 func RenderFormParsingErr(w http.ResponseWriter) {
 	http.Error(
 		w,
-		"There was an error in parsing your form. Please try again later",
+		"There was an error in parsing. Please use the provided user interface and try again.",
+		http.StatusBadRequest,
+	)
+}
+
+func RenderInvalidJSONErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"The provided data could not be parsed as valid JSON.",
+		http.StatusBadRequest,
+	)
+}
+
+func RenderInvalidTokenErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Invalid Token. Your request could not be processed. Please try again later",
+		http.StatusBadRequest,
+	)
+}
+
+func RenderSessionTemperedErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Your session appears to be tampered with. Please clear cookies and try again.",
+		http.StatusUnauthorized,
+	)
+}
+
+func RenderForbiddenAccessErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Forbidden access detected. Your request has been denied.",
+		http.StatusForbidden,
+	)
+}
+
+func RenderInvalidUserIDErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Invalid User ID. To manage users, please use the provided application interface.",
+		http.StatusBadRequest,
+	)
+}
+
+func RenderInvalidProjectIDErr(w http.ResponseWriter) {
+	http.Error(
+		w,
+		"Invalid Project ID. To manage projects, please use the provided application interface.",
 		http.StatusBadRequest,
 	)
 }
