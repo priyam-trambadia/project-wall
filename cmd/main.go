a6ac1eba0e5cff368b296986e9c3a23eb7b59b94
Priyamkumar Trambadia <priyamcancode@gmail.com>
2024-07-29 16:54:17 +0530

    Rename Package: mail to mailer

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/priyam-trambadia/project-wall/api/utils/jwt"
-	"github.com/priyam-trambadia/project-wall/internal/mail"
+	"github.com/priyam-trambadia/project-wall/internal/mailer"
 	"github.com/priyam-trambadia/project-wall/internal/models"
 )
 
 func main() {
 
 	jwt.LoadConfig()
-	mail.SetupMailer()
+	mailer.SetupMailer()
 
 	var cfg config
 	cfg.initConfig()
